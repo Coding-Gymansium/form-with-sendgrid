69639c79bd5ae63d810560d2bf2c5d6e9c4f5a19
Nico Rithner <nicorithner@gmail.com>
2021-01-18 18:05:19 -0700

    Add new and create actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,16 @@
 class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render.new
+    end
+  end
 end

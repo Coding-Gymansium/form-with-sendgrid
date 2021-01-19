6a64e5d7903df80a81584809a922ee2e535d3d56
Nico Rithner <nicorithner@gmail.com>
2021-01-18 20:55:36 -0700

    Fix controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,10 +4,10 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(contact_params)
+    @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      flash.now[:erro] = nil
     else
       flash.now[:error] = 'Cannot send message.'
       render.new

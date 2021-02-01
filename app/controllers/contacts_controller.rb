377690068f7b736c7db8ca04737b89d11715906f
Nico Rithner <nicorithner@gmail.com>
2021-01-31 19:14:57 -0700

    Change create to use sendgrid

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,19 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:error] = nil
-    else
-      flash.now[:error] = 'Cannot send message.'
-      render.new
-    end
+    # binding.pry
+    name = params[:contact][:name]
+    email = params[:contact][:email]
+    message = params[:contact][:message]
+    Sendgrid.send_mail(name, email, message)
+
+    # @contact = Contact.new(params[:contact])
+    # @contact.request = request
+    # if @contact.deliver
+    #   flash.now[:error] = nil
+    # else
+    #   flash.now[:error] = 'Cannot send message.'
+    #   render.new
+    # end
   end
 end
